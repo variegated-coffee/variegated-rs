88d902729b19d63e5e7884e22468956e441e82d6
Magnus Nordlander <magnus@nordlander.se>
2025-07-11 12:34:09 +0200

    Add Gpio Status Lambda Command sender

--- variegated-hal/src/gpio/gpio_command_sender.rs
+++ variegated-hal/src/gpio/gpio_command_sender.rs
@@ -1,8 +1,10 @@
+use alloc::boxed::Box;
+use defmt::info;
 use embassy_rp::gpio::Input;
 use embassy_sync::blocking_mutex::raw::RawMutex;
 use embassy_sync::channel::Sender;
+use embassy_sync::pubsub::Subscriber;
 use embassy_time::Timer;
-use embedded_hal_async::digital::Wait;
 use crate::WithTask;
 
 pub struct GpioCommandSender<'a, M: RawMutex, CommandT: Clone, const N: usize> {
@@ -44,6 +46,66 @@ impl <'a, M: RawMutex, CommandT: Clone, const N: usize> WithTask for GpioCommand
                 previous_level = false;
                 if let Some(ref falling_edge_command) = self.falling_edge_command {
                     self.sender.send(falling_edge_command.clone()).await;
+                }
+            }
+        }
+    }
+}
+
+pub struct GpioStatusLambdaCommandSender<'a, M: RawMutex, CommandT: Clone, StatusT: Clone, const N: usize, const SUBS: usize, const PUBS: usize> {
+    input: Input<'a>,
+    sender: Sender<'a, M, CommandT, N>,
+    status_subscriber: Subscriber<'a, M, StatusT, 1, SUBS, PUBS>,
+    rising_edge_lambda: Option<Box<dyn Fn(&StatusT) -> Option<CommandT>>>,
+    falling_edge_lambda: Option<Box<dyn Fn(&StatusT) -> Option<CommandT>>>,
+}
+
+impl<'a, M: RawMutex, CommandT: Clone, StatusT: Clone, const N: usize, const SUBS: usize, const PUBS: usize> GpioStatusLambdaCommandSender<'a, M, CommandT, StatusT, N, SUBS, PUBS> {
+    pub fn new(
+        input: Input<'a>,
+        sender: Sender<'a, M, CommandT, N>,
+        status_subscriber: Subscriber<'a, M, StatusT, 1, SUBS, PUBS>,
+        rising_edge_lambda: Option<Box<dyn Fn(&StatusT) -> Option<CommandT>>>,
+        falling_edge_lambda: Option<Box<dyn Fn(&StatusT) -> Option<CommandT>>>,
+    ) -> Self {
+        GpioStatusLambdaCommandSender {
+            input,
+            sender,
+            status_subscriber,
+            rising_edge_lambda,
+            falling_edge_lambda,
+        }
+    }
+}
+
+impl<'a, M: RawMutex, CommandT: Clone, StatusT: Clone, const N: usize, const SUBS: usize, const PUBS: usize> WithTask for GpioStatusLambdaCommandSender<'a, M, CommandT, StatusT, N, SUBS, PUBS> {
+    async fn task(&mut self) {
+        let mut previous_level = self.input.is_high();
+        let mut prev_state = self.status_subscriber.next_message_pure().await;
+        
+        loop {
+            self.input.wait_for_any_edge().await;
+            info!("Edge detected");
+            
+            Timer::after_millis(10).await;
+            
+            if let Some(s) = self.status_subscriber.try_next_message_pure() {
+                prev_state = s;
+            }
+            
+            if self.input.is_high() && !previous_level {
+                previous_level = true;
+                if let Some(ref lambda) = self.rising_edge_lambda {
+                    if let Some(command) = lambda(&prev_state) {
+                        self.sender.send(command).await;
+                    }
+                }
+            } else if !self.input.is_high() && previous_level {
+                previous_level = false;
+                if let Some(ref lambda) = self.falling_edge_lambda {
+                    if let Some(command) = lambda(&prev_state) {
+                        self.sender.send(command).await;
+                    }
                 }
             }
         }
